cc0b971a2f6eee267c2e9b9a31ec9719af8f28c1
Trent Keyes <tkeyes@gmail.com>
2022-10-26 18:05:47 -0500

    Updates

---
+++ precache-manifest.80f35671e50d2e04c6db30515874ba64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9554609d7a8a0c898ac",
+    "url": "/shopping-cart/static/css/main.101da955.chunk.css"
+  },
+  {
+    "revision": "a9554609d7a8a0c898ac",
+    "url": "/shopping-cart/static/js/main.932e263e.chunk.js"
+  },
+  {
+    "revision": "3675f54918f9d621420d",
+    "url": "/shopping-cart/static/js/runtime~main.60a85d2f.js"
+  },
+  {
+    "revision": "ab0f4f1d2536cc3f65c2",
+    "url": "/shopping-cart/static/js/2.9804d3b1.chunk.js"
+  },
+  {
+    "revision": "7c51237fb05f32ec246d2a302c1805db",
+    "url": "/shopping-cart/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.69f82484116d917822cd701feae47dae.js"
+  "/shopping-cart/precache-manifest.80f35671e50d2e04c6db30515874ba64.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-app/index.html", {
+workbox.routing.registerNavigationRoute("/shopping-cart/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
