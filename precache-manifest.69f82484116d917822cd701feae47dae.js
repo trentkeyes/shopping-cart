660064f430838b1ef1f7071d168979a71f1fa956
Trent Keyes <tkeyes@gmail.com>
2022-10-26 10:19:30 -0500

    Updates

---
+++ precache-manifest.69f82484116d917822cd701feae47dae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9554609d7a8a0c898ac",
+    "url": "/my-app/static/css/main.101da955.chunk.css"
+  },
+  {
+    "revision": "a9554609d7a8a0c898ac",
+    "url": "/my-app/static/js/main.932e263e.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "ab0f4f1d2536cc3f65c2",
+    "url": "/my-app/static/js/2.9804d3b1.chunk.js"
+  },
+  {
+    "revision": "cd58cbc190616daaf720be23031b4900",
+    "url": "/my-app/index.html"
+  }
+];
